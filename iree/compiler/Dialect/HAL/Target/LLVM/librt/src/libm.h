42788f6ef67d0258e8e95a61be432474cc0e97d5
Eric Eaton <ereaton@fb.com>
2022-02-16 18:51:29 -0800

    Add the fp16 <-> fp32 conversion methods to librt
    The 16 bit ops when lowered through the LLVM path use __gnu_h2f_ieee
    and __gnh_f2h_ieee methods. Import an implementation of these.

--- iree/compiler/Dialect/HAL/Target/LLVM/librt/src/libm.h
+++ iree/compiler/Dialect/HAL/Target/LLVM/librt/src/libm.h
@@ -11,5 +11,7 @@
 
 // https://en.cppreference.com/w/c/numeric/math/fma
 LIBRT_EXPORT float fmaf(float x, float y, float z);
+LIBRT_EXPORT short __gnu_f2h_ieee(float param);
+LIBRT_EXPORT float __gnu_h2f_ieee(short param);
 
 #endif  // IREE_COMPILER_DIALECT_HAL_TARGET_LLVM_LIBRT_SRC_LIBM_H_
